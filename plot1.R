922a3d75ac3bd08abc582852e586336384b17b9e
ashic <ashic@live.com>
2014-05-11 19:09:13 +0100

    code ready...readme next

--- plot1.R
+++ plot1.R
@@ -1,3 +1,8 @@
+#This script exposes a function plot1() that renders the first plot.
+#It also calls the function as the question asks us to render the png from the script.
+
+
+#import data loading, etc. utility functions
 source('utility.R')
 
 plot1 <- function(){
@@ -5,13 +10,15 @@ plot1 <- function(){
     
     data <- getData()
     
-    gap <- data$Global_active_power
-    
+    #create output directory if needed.
     dir.create('out', showWarnings='F')
     
+    #plot to png
     png('out/plot1.png', width=480, height=480, bg='transparent')
-    hist(gap, main="Global Active Power", xlab="Global Active Power (kilowatts)", col='red')
+    hist(data$Global_active_power, main="Global Active Power", 
+         xlab="Global Active Power (kilowatts)", col='red')
     dev.off()
 }
 
+#carry out the plot.
 plot1()
